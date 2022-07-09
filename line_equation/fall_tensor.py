e7570e16100218de20ed2eb85ac607210ad1cd10
u59501 <u59501@login-2.aidevcloud>
2022-07-09 07:47:28 -0700

    files from intel devcloud

---
+++ line_equation/fall_tensor.py
@@ -0,0 +1,70 @@
+"""
+
+##########################################
+
+# @title: DIRECTION FIELD VISUALIZATION
+
+# @author: Oluwatosin Odubanjo
+
+# @email: olutosinbanjo@gmail.com
+
+# @date: July 08, 2022
+
+#########################################
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import time
+
+def differential_equation(v):
+    dv = 9.8 - (v / 5)
+    return dv
+
+def line_equation(x1,y1,m,x):
+    y = m * (x - x1) + y1
+    return y
+
+def equilibrium_solution(y_ax):
+    slope_array = np.zeros([y_ax.size])
+    for i in range (y_ax.size): #can also use(len(y_ax)):
+        for j in y_ax:
+            slope_array[i] = differential_equation(j)
+            if(slope_array[i] == 0):
+                plt.axhline(y=j, color='black', linestyle='-')
+    plt.show()
+        
+def solutions(x_ax, y_ax):
+    graph_function = "v\'(t) = 9.8 - (v/5)"
+    for i in x_ax:
+        for j in y_ax:    
+            slope = differential_equation(j)
+            X = tf.constant(np.linspace(i-0.2, i+0.2, 2))
+            Y = line_equation(i, j, slope, X)
+            plt.plot(X, Y, color='black')
+    plt.title(f"Direction Field for {graph_function}")
+    equilibrium_solution(y_ax)
+    plt.show()
+
+def main():
+
+    graph_function = "v\'(t) = 9.8 - (v/5)"
+
+    # define points for x and y axis
+    nx, ny = 1, 1
+    x_axis = np.arange(1, 11, nx) #tf.experimental.numpy.arange(1, 11, nx)
+    y_axis = np.arange(20, 60, ny) #tf.experimental.numpy.arange(20, 60, ny)
+
+    # plot direction field
+    start = time.perf_counter()
+    solutions(x_axis, y_axis)
+    end = time.perf_counter()
+    time_elapsed = end - start
+    plt.savefig("plot.png")
+    print(f"Time taken to plot direction field for {graph_function} = {time_elapsed} seconds")
+
+if __name__ == "__main__":
+    main()
+
---
+++ quiver/fall_dpctl_cpu.py
@@ -0,0 +1,87 @@
+"""
+
+##################################################
+
+@title: Direction Field Visualization
+
+@author: Oluwatosin Odubanjo
+
+@email: olutosinbanjo@gmail.com
+
+@date: July 08, 2022
+
+###################################################
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# this function allows the output display elements in my array for debugging purposes
+#np.set_printoptions(threshold=10**6)
+
+
+def equilibrium_solution(dy, Y):
+    round_dy = np.round(dy, decimals=6)         # round to 6 decimal place for accurate location of solution
+    zero_index = np.where(round_dy == 0.000000) # get index of zero elements, 0.000000 serves as the zero bound limit
+    equilibrium_solution = Y[zero_index]        # get the values of Y which makes dy == 0
+   #print(equilibrium_solution)
+    
+    # Plot equilibrium position
+    plt.axhline(y=equilibrium_solution[0], color='black', linestyle='-')
+
+    #plt.show
+
+
+def other_solutions(X, Y, dx, dy, function):
+
+    # Plot other solutions
+    plt.quiver(X, Y, dx, dy, color='black', headaxislength=5, headlength=0,
+                pivot='middle', scale=2, linewidth=.2, units='xy', width = .05, headwidth=1)
+    plt.title(f'Direction Field and Equilibrium Solution for {function}')
+    #plt.show()
+    
+def main():
+
+    # define points for x and y axis for graph
+    nt, nv = 1 , 1      
+    t = np.arange(1, 11, nt)    
+    v = np.arange(20, 60, nv)   
+
+    # rectangular grid with points
+    T, V = np.meshgrid(t, v)
+
+    # derivative
+    dv = 9.8 - (V / 5)
+    dt = np.ones(dv.shape)
+
+    #string to input function in title of the graph
+    graph_function = "v\'(t) = 9.8 - v/5" 
+    
+    # Plot direction Field and equilibrium position
+    start = time.perf_counter()
+    equilibrium_solution(dv, V)
+    other_solutions(T, V, dt, dv, graph_function)
+    end = time.perf_counter()
+    time_elapsed = end - start
+
+    plt.savefig("plot_dpctl_cpu.png")
+
+    print(f"Time taken to plot function: {time_elapsed} seconds")
+
+if __name__ == "__main__":
+
+    try:
+        import dpctl
+
+        with dpctl.device_context("opencl:cpu") as cpu_queue:
+            print("Device :\n")
+            cpu_queue.print_device_info()
+            print("Plotting with data parallel control on cpu device...\n")
+            main()
+
+    except ImportError:
+        print("Plotting without data parallel control on host device...\n")
+        main()
+        
