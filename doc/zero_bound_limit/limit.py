46ff783d0f6f3c70348586e17aa0053327fa784d
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-07-15 07:32:40 +0100

    Update limit.py

--- doc/zero_bound_limit/limit.py
+++ doc/zero_bound_limit/limit.py
@@ -1,3 +1,43 @@
+"""
+@date: original thought: July 8, 2022.
+       properly formed: July 15, 2022.
+@author: Oluwatosin Odubanjo
+
+The Safe Zero-bound Limit
+
+The term Safe Zero-bound Limit is an adaptation of the Economics term zero-bound limit.
+
+This term is derived in order to help me obtain the equilibrium solution of the differential equation computationally.
+
+Meaning :
+
+```
+If `a` conatins k number of elements and 
+there is an element of the power E-n; where n is a integer.
+This integer shall determine the `the zero-bound element`;
+The zero-bound element shall impose on other elements in `a`,
+a standard for filtering `a`.
+
+This standard shall be called the `safe zero-bound limit`. 
+The safe zero-bound limit shall be a user-defined integer,
+greater than or equal to 5, to maintain a degree of accuracy
+(such as : 0E-5 == `5` decimal places, 0E-6 == `6` decimal places, e.t.c).
+Then all elements in `a` shall be rounded this limit and no further;
+and `a` shall be filtered to obtain the zero-bound element.
+
+In the case of obtaining the equilibrium solution:
+The zero-bound limit determines the cut-down / round condition for the elements of the array 
+and therefore allows the filtering of an array in order to obtain the equilibrium solution.
+
+"""
+
+"""
+For example:
+Assuming that the element `6` in array `a` defined in main() function is
+the equilibrium solution, we can apply the safe zero-bound limit method
+in locating it.
+"""
+
 import numpy as np
 
 def without_zero_bound_limit(array):
@@ -7,14 +47,6 @@ def without_zero_bound_limit(array):
     print("Original array: ", array, '\n')
     print("Zero Index: ", zero_index, '\n')
     print("Number at zero index: ", equilibrium_solution, '\n')
-
-
-"""
-To get the zero index
-Introduce a zero-bound limit (= 0E-6 == rounding to 6 decimal places)
-We round the values to this limit and no further. 
-and then filter the array to obtain the zero index
-"""
 
 def with_zero_bound_limit(array):
     array_round = np.round(array, decimals=6)
