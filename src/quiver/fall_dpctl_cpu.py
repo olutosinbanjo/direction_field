33dd9d6e0f7632eee8f56f45d46804b2dc82a882
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-07-21 23:53:17 +0100

    Update fall_dpctl_cpu.py

--- src/quiver/fall_dpctl_cpu.py
+++ src/quiver/fall_dpctl_cpu.py
@@ -1,10 +1,27 @@
+#                      Direction Field Visualization with Python
+#
+# Copyright 2022 Oluwatosin Odubanjo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """
 
-##################################################
+####################################################
 
 @title: Direction Field Visualization
 
-@package: numpy + dpctl{cpu}
+@method: matplotlib.pyplot.quiver (MPQ) + dpctl{cpu}
 
 @author: Oluwatosin Odubanjo
 
@@ -12,7 +29,7 @@
 
 @date: July, 2022
 
-###################################################
+#####################################################
 
 """
 
