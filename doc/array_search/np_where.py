2babf927855ed51f043fd016c0a73966805f66d0
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-07-22 15:08:28 +0100

    Update np_where.py

--- doc/array_search/np_where.py
+++ doc/array_search/np_where.py
@@ -5,41 +5,46 @@
 ##########################################
 """
 
+#############
+
 import numpy as np
 import time
 
-def find_solution(a, position):
+def find_solution(a):
     index = np.where(a%2 == 0)
-    solution = position[index]
 
-    return solution
+    return index
+
+def linear_search(a):
+    check = a % 2
+    for i in range (len(check)):
+        if check[i] == 0:
+            return i
 
 def main():
 
     a = np.random.randint(1, 1000, 100000)
-    position_a = np.arange(0, 100000)
 
     b = np.random.randint(1, 1000, 500000)
-    position_b = np.arange(0, 500000)
 
     c = np.random.randint(1, 1000, 10000000)
-    position_c = np.arange(0, 10000000)
 
     start = time.perf_counter()
-    result = find_solution(a, position_a)
+    result = find_solution(a)  # linear_search(a)
     end = time.perf_counter()
     diff = end - start
 
     start_b = time.perf_counter()
-    result_b = find_solution(b, position_b)
+    result_b = find_solution(b) # linear_search(b)
     end_b = time.perf_counter()
     diff_b = end_b - start_b
 
     start_c = time.perf_counter()
-    result_c = find_solution(c, position_c)
+    result_c = find_solution(c) # linear_search(c)
     end_c = time.perf_counter()
     diff_c = end_c - start_c
 
+    # timing in seconds
     print('time taken : 100000 = ', diff)
     print('time taken : 500000 = ', diff_b)
     print('time taken : 10000000 = ', diff_c)
